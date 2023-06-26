f6e19341097533b2c94ffbb04ca32d23c1391eb9
HeesooHan <hhs950824@gmail.com>
2023-06-26 18:10:02 +0900

    first commit

---
+++ src/main/java/com/example/toys_servlet/ToysServletApplication.java
@@ -0,0 +1,13 @@
+package com.example.toys_servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToysServletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toys_servlet/ToysServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toys_servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToysServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
