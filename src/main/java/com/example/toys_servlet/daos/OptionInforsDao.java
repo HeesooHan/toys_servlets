67bd9bfb2eb47f424d6e96655795f9d486b56a01
hhyowon <hyowon0605@gmail.com>
2023-06-27 17:41:38 +0900

    push

--- src/main/java/com/example/toys_servlet/daos/OptionInforsDao.java
+++ src/main/java/com/example/toys_servlet/daos/OptionInforsDao.java
@@ -8,14 +8,36 @@ import java.util.HashMap;
 import com.example.toys_servlet.commons.Commons;
 
 public class OptionInforsDao {
-    // unique_id를 기준으로 OptionInfors 테이블에서 레코드를 삭제하는 메서드
+    //상세보기를 위한 select
+        public HashMap SelectWithUniqueId(String unique_id) {
+        HashMap optionInforRecord = new HashMap<>();
+        try {
+            Commons commons = new Commons();
+            Statement statement = commons.getStatement();
+            String query = "SELECT * FROM respondents WHERE RESPONDENTS_ID = '" + unique_id + "';";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            if (resultSet.next()) {
+                optionInforRecord.put("RESPONDENTS_ID", resultSet.getString("RESPONDENTS_ID"));
+                optionInforRecord.put("RESPONDENTS", resultSet.getString("RESPONDENTS"));
+                optionInforRecord.put("PASSWORD", resultSet.getString("PASSWORD"));
+            }
+
+            resultSet.close();
+            statement.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return optionInforRecord;
+    }
+
     public static int DeleteWithUniqueID(String unique_id) {
         int count = 0;
         try {
             Commons commons = new Commons();
             Statement statement = commons.getStatement(); // workbench에서 작성한 쿼리를 실행하는 객체
-            String query = "DELETE FROM option_infors\n" + //
-                    "where OPTION_INFOR_ID = '"+unique_id+"';"; // unique_id를 기준으로 레코드 삭제하는 쿼리
+            String query = "DELETE FROM respondents\n" + //
+                    "where RESPONDENTS_ID = '"+unique_id+"';"; // unique_id를 기준으로 레코드 삭제하는 쿼리
             count = statement.executeUpdate(query); // 쿼리 실행 결과로 영향 받은 레코드의 개수를 반환
         } catch (Exception e) {
             System.out.println(e.getMessage()); // 예외 발생 시 메시지 출력
@@ -33,15 +55,15 @@ public class OptionInforsDao {
             Commons commons = new Commons();
             Statement statement = commons.getStatement(); // workbench에서 작성한 쿼리를 실행하는 객체
             String query = "SELECT *\n" + //
-                    "FROM option_infors\n" + //
-                    "WHERE OPTION_NAME like '" + search + "%';"; // search를 포함하는 OPTION_NAME 레코드를 조회하는 쿼리
+                    "FROM respondents\n" + //
+                    "WHERE RESPONDENTS like '" + search + "%';"; // search를 포함하는 RESPONDENTS 레코드를 조회하는 쿼리
             ResultSet resultSet = statement.executeQuery(query); // 쿼리 실행 결과로 ResultSet 객체 반환
 
             HashMap optionInforRecord = new HashMap<>(); // 조회된 각 레코드를 담을 HashMap 객체 생성
             while (resultSet.next()) { // ResultSet 객체의 레코드를 순회
                 optionInforRecord = new HashMap<>(); // 새로운 HashMap 객체 생성
-                optionInforRecord.put("OPTION_INFOR_ID", resultSet.getString("OPTION_INFOR_ID")); // OPTION_INFOR_ID를 키로하여 값을 저장
-                optionInforRecord.put("OPTION_NAME", resultSet.getString("OPTION_NAME")); // OPTION_NAME을 키로하여 값을 저장
+                optionInforRecord.put("RESPONDENTS_ID", resultSet.getString("RESPONDENTS_ID")); // RESPONDENTS_ID를 키로하여 값을 저장
+                optionInforRecord.put("RESPONDENTS", resultSet.getString("RESPONDENTS")); // RESPONDENTS을 키로하여 값을 저장
 
                 optionInforList.add(optionInforRecord); // 조회된 레코드를 optionInforList에 추가
             }
